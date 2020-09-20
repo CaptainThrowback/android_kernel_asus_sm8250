5f7697175356a5785446f421f1ec0e6cd7305a28
Captain Throwback <captainthrowback@hotmail.com>
2020-09-20 12:08:00 -0400

    ZS670KS/ZS671KS Kernel Source Code V29.10.22.32
    2020/08/14  508.13 MBytes

---
+++ kernel/ship_prebuilt/primary_kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Aug 11 10:37:24 CST 2020"
+#define LINUX_COMPILE_BY "builder845"
+#define LINUX_COMPILE_HOST "builder845"
+#define LINUX_COMPILER "clang version 8.0.12 for Android NDK"
